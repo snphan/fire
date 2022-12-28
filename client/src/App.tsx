c43809e099685dc15d584512a8ae9cb5a96329aa
Steven Phan <snphan@ualberta.ca>
2022-12-28 01:00:37 -0700

    add basic sign in and sign out with google

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getModeForUsageLocation } from 'typescript';
+import { REACT_APP_GOOGLE_CREDS_APPID } from './config';
+import jwt_decode from 'jwt-decode';
+
+declare global {
+  /* google variable is loaded from script in public/index.html */
+  const google: any;
+}
 
 function App() {
+
+  const [user, setUser] = useState<any>(undefined);
+
+  function handleCallbackResponse(res: any) {
+    console.log(res);
+    console.log("Encoded JWT ID token: " + res.credential);
+    let userObject = jwt_decode(res.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv")!.hidden = true;
+  }
+
+  function handleSignOut() {
+    setUser(undefined);
+    document.getElementById("signInDiv")!.hidden = false;
+  }
+
+  useEffect(() => {
+    /* Google Authentication */
+    google.accounts.id.initialize({
+      client_id: REACT_APP_GOOGLE_CREDS_APPID,
+      callback: handleCallbackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large" }
+    );
+
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +57,14 @@ function App() {
         >
           Learn React
         </a>
+        <div id="signInDiv"></div>
+        {user &&
+          <div>
+            <img src={user.picture} alt="" />
+            <h3>Welcome {user.name}!</h3>
+            <button onClick={() => handleSignOut()}>Sign Out</button>
+          </div>
+        }
       </header>
     </div>
   );
