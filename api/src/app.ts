cf7fe3254a2755e1447549f1076e08c2d4d48abf
Steven Phan <snphan@ualberta.ca>
2022-12-29 21:36:36 -0700

    fix CORS error

--- api/src/app.ts
+++ api/src/app.ts
@@ -55,7 +55,7 @@ class App {
       this.app.use(helmet());
     }
 
-    this.app.use(cors({ origin: ['https://studio.apollographql.com', 'http://localhost:3000'], credentials: CREDENTIALS }));
+    this.app.use(cors({ origin: ['https://studio.apollographql.com', 'http://localhost:8000', 'http://localhost:3000'], credentials: CREDENTIALS }));
     this.app.use(compression());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
