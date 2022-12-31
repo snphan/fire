39a9d9654422f553b0991378a1fb44755da445b9
Steven Phan <snphan@ualberta.ca>
2022-12-31 15:02:13 -0700

    add custom uuid names and include in the response

--- upload/src/app.ts
+++ upload/src/app.ts
@@ -1,14 +1,30 @@
 import express from 'express';
 import multer from 'multer';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 
-const upload = multer({ dest: "./media" });
 
 app.use("/media", express.static("./media"));
 
-app.post("/upload", upload.array('file'), (req, res) => {
-  res.json({ status: "success" });
+/* Multer Settings */
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "media");
+  },
+  filename: (req, file, callback) => {
+    /* Custom UUID file name */
+    const { originalname } = file;
+    callback(null, `${uuidv4()}-${originalname}`);
+  }
+})
+
+const upload = multer({ storage });
+
+app.post("/upload", upload.array('file'), (req: any, res) => {
+  let names = [];
+  req.files.forEach(file => names.push(file.filename));
+  res.json({ status: "success", filenames: names });
 })
 
 app.listen(4000, () => console.log("listening on port 4000"));
