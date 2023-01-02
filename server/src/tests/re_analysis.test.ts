0525de5631f992d4eb4e0a06037dcde59045b7ac
Steven Phan <snphan@ualberta.ca>
2023-01-02 12:29:54 -0700

    start real estate asset creation test

--- server/src/tests/re_analysis.test.ts
+++ server/src/tests/re_analysis.test.ts
@@ -4,6 +4,7 @@ import { CreateUserDto, UserLoginDto } from '@dtos/users.dto';
 import { authResolver } from '@/resolvers/auth.resolver';
 import { userResolver } from '@/resolvers/users.resolver';
 import { getConnection } from 'typeorm';
+import { CreateREAssetDto } from '@/dtos/re_asset.dto';
 
 let app: App;
 let userId: number;
@@ -31,7 +32,7 @@ afterAll(async () => {
 
 
 
-describe('Testing Auth', () => {
+describe('Testing Real Estate Asset Analysis', () => {
   describe('[POST] /graphql', () => {
     it('response should have the Create userData', async () => {
       const userData: CreateUserDto = {
@@ -55,7 +56,44 @@ describe('Testing Auth', () => {
       expect(response.error).toBeFalsy();
       expect(response.body.data.createUser.email).toBe(userData.email);
       userId = response.body.data.createUser.id;
+    });
+  });
+  describe('[POST] create real estate asset /graphql', () => {
+    it('should return some of the real estate asset data', async () => {
 
+
+      const REAssetData: CreateREAssetDto = {
+        userId: userId,
+        purchase_price: 1000,
+        address: "Some Fake Address",
+        postal_code: "123-2456",
+        city: "some city",
+        province: "some province",
+        country: "some country",
+        picture_links: ["123_img.png", "3454_img.png"],
+        purchase_date: Date(),
+        hold_length: 10,
+        favorite: true,
+        tracking: true
+      };
+
+      const createREAssetData = {
+        query: `mutation createREAsset($REAssetData: CreateREAssetDto!) {
+                  createREAsset(REAssetData: $REAssetData) {
+                    user
+                    purchase_price
+                    address
+                    postal_code
+                  }
+                }`,
+        variables: { REAssetData: REAssetData }
+      }
+
+      const response = await request(app.getServer()).post('/graphql').send(createREAssetData);
+      expect(response.error).toBeFalsy();
+      expect(response.body.data.createREAsset.user).toBe(REAssetData.userId);
+      expect(response.body.data.createREAsset.address).toBe(REAssetData.address);
+      expect(response.body.data.createREAsset.purchase_price).toBe(REAssetData.purchase_price);
     });
   });
 })
