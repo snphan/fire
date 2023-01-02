ee883d90c60877cd20ee6581bfacc3b9bf12f6a0
Steven Phan <snphan@ualberta.ca>
2023-01-01 20:36:50 -0700

    start tables for re_tracking

---
+++ server/src/entities/re_assumption.entity.ts
@@ -0,0 +1,3 @@
+import { IsNotEmpty } from 'class-validator';
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Field, ObjectType } from 'type-graphql';
