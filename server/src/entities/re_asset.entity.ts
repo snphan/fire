df3ae4b5e9df978a1fde56dac2bc6d1e3e08da4f
Steven Phan <snphan@ualberta.ca>
2023-01-02 11:10:57 -0700

    init re_asset and re_receipt entities

--- server/src/entities/re_asset.entity.ts
+++ server/src/entities/re_asset.entity.ts
@@ -1,3 +1,17 @@
 import { IsNotEmpty } from 'class-validator';
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, CreateDateColumn, UpdateDateColumn, ManyToOne, OneToMany } from 'typeorm';
 import { Field, ObjectType } from 'type-graphql';
+import { REReceipt } from './re_receipt.entity';
+
+@ObjectType()
+@Entity()
+export class REAsset extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @OneToMany((type) => REReceipt, re_receipt => re_receipt.re_asset, {
+    onDelete: "SET NULL"
+  })
+  re_receipts: REReceipt[];
+}
--- server/src/entities/re_receipt.entity.ts
+++ server/src/entities/re_receipt.entity.ts
@@ -1,3 +1,35 @@
 import { IsNotEmpty } from 'class-validator';
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, CreateDateColumn, UpdateDateColumn, OneToMany, ManyToOne } from 'typeorm';
 import { Field, ObjectType } from 'type-graphql';
+import { REAsset } from './re_asset.entity';
+
+@ObjectType()
+@Entity()
+export class REReceipt extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne((type) => REAsset, re_asset => re_asset.re_receipts, {
+    onDelete: "SET NULL"
+  })
+  re_asset: REAsset;
+
+  @Field()
+  @Column()
+  @IsNotEmpty()
+  timestamp: number; /* Unix Time */
+
+  @Field({ nullable: false })
+  @Column({ nullable: false, default: "gas" })
+  @IsNotEmpty()
+  type: string;
+
+  @Field(type => [String])
+  @Column("text", { array: true })
+  receipt_link: string[];
+
+  @Field({ nullable: true })
+  @Column({ nullable: true })
+  notes?: string;
+}
