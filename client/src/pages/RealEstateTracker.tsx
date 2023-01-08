13588f20212552a64d0e65352ab895327b8891b5
Steven Phan <snphan@ualberta.ca>
2023-01-08 15:34:00 -0700

    add button to add realestate asset

--- client/src/pages/RealEstateTracker.tsx
+++ client/src/pages/RealEstateTracker.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { GET_USER_BY_ID } from '@/queries';
 import { useLazyQuery, useQuery } from '@apollo/client';
 import { REListItem } from '@/components/REListItem';
+import { Tooltip } from '@material-tailwind/react';
 
 
 export function RealEstateTracker({ userID }: any) {
@@ -17,9 +18,12 @@ export function RealEstateTracker({ userID }: any) {
 
   return (
     <div className="ml-24 min-h-screen">
-      <div className="flex ml-2">
+      <div className="flex items-center ml-2">
         <h1>Real Estate Tracker</h1>
         <div className="grow"></div>
+        <Tooltip content={"Add"} className="capitalize bg-zinc-900 p-2">
+          <span className="m-4 text-zinc-600 hover:text-zinc-200 cursor-pointer text-5xl material-icons material-symbols-outlined">add</span>
+        </Tooltip>
       </div>
       <div className="grid grid-cols-1 xl:grid-cols-2 gap-2">
         {REAssetData?.getUserById.re_asset.map((item: any) => {
