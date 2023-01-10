455464f4659ccefc194a2e09282cc61976d20a46
Steven Phan <snphan@ualberta.ca>
2023-01-09 19:20:10 -0700

    update the create reasset form

--- client/src/components/AddREAssetForm.tsx
+++ client/src/components/AddREAssetForm.tsx
@@ -26,6 +26,8 @@ interface REAsset {
   hold_length: number;
   favorite: boolean;
   tracking: boolean;
+  bedrooms: number;
+  bathrooms: number;
 }
 
 
@@ -49,7 +51,9 @@ export function AddREAssetForm({ open, handleOpen, userID }: any) {
     purchase_date: new Date(),
     hold_length: 10,
     favorite: false,
-    tracking: false
+    tracking: false,
+    bedrooms: 0,
+    bathrooms: 0
   }
 
   const uploadFiles = (files: FileList) => {
@@ -69,7 +73,7 @@ export function AddREAssetForm({ open, handleOpen, userID }: any) {
   const [REAssetInfo, setREAssetInfo] = useState<REAsset>(JSON.parse(JSON.stringify(defaultREAsset)));
 
   return (
-    <Dialog size="xl" open={open} handler={handleOpen} className="bg-gray-800 max-h-screen overflow-auto">
+    <Dialog size="lg" open={open} handler={handleOpen} className="bg-gray-800 max-h-screen overflow-auto">
       <DialogHeader className="text-gray-100">Add a Property</DialogHeader>
       <DialogBody className="text-gray-200 flex flex-col">
 
@@ -96,16 +100,31 @@ export function AddREAssetForm({ open, handleOpen, userID }: any) {
 
         <div className="flex flex-wrap mx-3 mb-6">
           <label className="block uppercase tracking-wide text-gray-500 text-xs font-bold mb-2"
+            htmlFor="Purchase Price">Purchase Price</label>
+          <input className="appearance-none block w-full text-lg bg-gray-200 text-gray-700 border border-gray-200 rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            type="text" name="" id="Purchase Price" value={REAssetInfo.purchase_price} onChange={(e) => setREAssetInfo(({ ...REAssetInfo, purchase_price: e.target.value ? parseInt(e.target.value) : 0 }))} />
+        </div>
+
+        <div className="flex flex-wrap mx-3 mb-6">
+          <label className="block uppercase tracking-wide text-gray-500 text-xs font-bold mb-2"
             htmlFor="Address">Address</label>
           <input className="appearance-none block w-full text-lg bg-gray-200 text-gray-700 border border-gray-200 rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             type="text" name="" id="Address" value={REAssetInfo.address} onChange={(e) => setREAssetInfo(({ ...REAssetInfo, address: e.target.value }))} />
         </div>
 
-        <div className="flex flex-wrap mx-3 mb-6">
-          <label className="block uppercase tracking-wide text-gray-500 text-xs font-bold mb-2"
-            htmlFor="Purchase Price">Purchase Price</label>
-          <input className="appearance-none block w-full text-lg bg-gray-200 text-gray-700 border border-gray-200 rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            type="text" name="" id="Purchase Price" value={REAssetInfo.purchase_price} onChange={(e) => setREAssetInfo(({ ...REAssetInfo, purchase_price: e.target.value ? parseInt(e.target.value) : 0 }))} />
+        <div className="flex">
+          <div className="flex flex-wrap mx-3 mb-6">
+            <label className="block uppercase tracking-wide text-gray-500 text-xs font-bold mb-2"
+              htmlFor="bedrooms">bedrooms</label>
+            <input className="appearance-none block w-full text-lg bg-gray-200 text-gray-700 border border-gray-200 rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              type="number" name="" id="bedrooms" value={REAssetInfo.bedrooms} onChange={(e) => setREAssetInfo(({ ...REAssetInfo, bedrooms: e.target.value ? parseInt(e.target.value) : 0 }))} />
+          </div>
+          <div className="flex flex-wrap mx-3 mb-6">
+            <label className="block uppercase tracking-wide text-gray-500 text-xs font-bold mb-2"
+              htmlFor="bathrooms">bathrooms</label>
+            <input className="appearance-none block w-full text-lg bg-gray-200 text-gray-700 border border-gray-200 rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              type="number" name="" id="bathrooms" value={REAssetInfo.bathrooms} onChange={(e) => setREAssetInfo(({ ...REAssetInfo, bathrooms: e.target.value ? parseInt(e.target.value) : 0 }))} />
+          </div>
         </div>
 
         <div className="flex">
@@ -136,9 +155,8 @@ export function AddREAssetForm({ open, handleOpen, userID }: any) {
             <label className="block uppercase tracking-wide text-gray-500 text-xs font-bold mb-2"
               htmlFor="hold_length">Hold Length</label>
             <input className="appearance-none block w-full text-lg bg-gray-200 text-gray-700 border border-gray-200 rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              type="text" name="" id="hold_length" value={REAssetInfo.hold_length} onChange={(e) => setREAssetInfo(({ ...REAssetInfo, hold_length: e.target.value ? parseInt(e.target.value) : 0 }))} />
+              type="number" name="" id="hold_length" value={REAssetInfo.hold_length} onChange={(e) => setREAssetInfo(({ ...REAssetInfo, hold_length: e.target.value ? parseInt(e.target.value) : 0 }))} />
           </div>
-
           <div className="flex flex-wrap mx-3 mb-6">
             <label className="block uppercase tracking-wide text-gray-500 text-xs font-bold mb-2"
               htmlFor="purchase_date">Purchase Date</label>
--- client/src/queries.ts
+++ client/src/queries.ts
@@ -17,6 +17,8 @@ export const GET_USER_BY_ID = gql`
         id
         address
         purchase_price
+        bedrooms
+        bathrooms
         picture_links
         city
         province
