78d48fff6e82e5bc839b03b30bf11be9722c34b5
Steven Phan <snphan@ualberta.ca>
2023-01-14 10:49:11 -0700

    parsefloat the assumptions input

--- client/src/components/AddREAssumptionsForm.tsx
+++ client/src/components/AddREAssumptionsForm.tsx
@@ -11,10 +11,14 @@ import { CREATE_REASSET } from '@/mutations';
 import { GET_USER_BY_ID } from '@/queries';
 
 
-export function AddREAssumptionsForm({ assumptions }: any) {
+export function AddREAssumptionsForm({ currentAsset, setCurrentAsset, assumptions }: any) {
 
-  const [newAssumptions, setNewAssumptions] = useState(JSON.parse(JSON.stringify(assumptions)));
+  const [newAssumptions, setNewAssumptions] = useState(JSON.parse(JSON.stringify(assumptions))); // create a state so that we can directly update assumptions.
   const otherExpensePlaceholder = JSON.parse(JSON.stringify(newAssumptions.other_expenses));
+
+  useEffect(() => {
+    setCurrentAsset({ ...currentAsset, re_assumptions: newAssumptions })
+  }, [newAssumptions])
 
   return (
     <div className="flex flex-col">
@@ -34,30 +38,30 @@ export function AddREAssumptionsForm({ assumptions }: any) {
 
       <div className="ml-4">Up-Front Costs</div>
       <div className="grid grid-cols-3 gap-4 p-4">
-        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, closing_cost: e.target.value })} value={newAssumptions.closing_cost} type="number" variant="outlined" label="Closing Costs" className="!text-gray-300" ></Input>
-        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, down_percent: e.target.value })} value={newAssumptions.down_percent} type="number" variant="outlined" label="Down Percent" className="!text-gray-300" ></Input>
+        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, closing_cost: parseFloat(e.target.value) })} value={newAssumptions.closing_cost} type="number" variant="outlined" label="Closing Costs" className="!text-gray-300" ></Input>
+        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, down_percent: parseFloat(e.target.value) })} value={newAssumptions.down_percent} type="number" variant="outlined" label="Down Percent" className="!text-gray-300" ></Input>
       </div>
 
       <div className="ml-4">Market Forces</div>
       <div className="grid grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, inflation: e.target.value })} value={newAssumptions.inflation} type="number" variant="outlined" label="Inflation %" className="!text-gray-300" ></Input>
-        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, interest_rate: e.target.value })} value={newAssumptions.interest_rate} type="number" variant="outlined" label="Interest Rate %" className="!text-gray-300" ></Input>
-        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, property_inc: e.target.value })} value={newAssumptions.property_inc} type="number" variant="outlined" label="Property Increase %" className="!text-gray-300" ></Input>
-        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, rent_inc: e.target.value })} value={newAssumptions.rent_inc} type="number" variant="outlined" label="Rent Increase %" className="!text-gray-300" ></Input>
+        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, inflation: parseFloat(e.target.value) })} value={newAssumptions.inflation} type="number" variant="outlined" label="Inflation %" className="!text-gray-300" ></Input>
+        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, interest_rate: parseFloat(e.target.value) })} value={newAssumptions.interest_rate} type="number" variant="outlined" label="Interest Rate %" className="!text-gray-300" ></Input>
+        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, property_inc: parseFloat(e.target.value) })} value={newAssumptions.property_inc} type="number" variant="outlined" label="Property Increase %" className="!text-gray-300" ></Input>
+        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, rent_inc: parseFloat(e.target.value) })} value={newAssumptions.rent_inc} type="number" variant="outlined" label="Rent Increase %" className="!text-gray-300" ></Input>
 
       </div >
 
       <div className="ml-4">Operation</div>
       <div className="grid grid-cols-3 lg:grid-cols-4 gap-4 p-4 overflow-hidden">
-        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, rent: e.target.value })} value={newAssumptions.rent} type="number" variant="outlined" label="Rent" className="!text-gray-300" ></Input>
-        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, insurance: e.target.value })} value={newAssumptions.insurance} type="number" variant="outlined" label="Insurance" className="!text-gray-300" ></Input>
-        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, maintenance_fee: e.target.value })} value={newAssumptions.maintenance_fee} type="number" variant="outlined" label="Maintenance Fee (Month)" className="!text-gray-300" ></Input>
-        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, utilities: e.target.value })} value={newAssumptions.utilities} type="number" variant="outlined" label="Utilities" className="!text-gray-300" ></Input>
+        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, rent: parseFloat(e.target.value) })} value={newAssumptions.rent} type="number" variant="outlined" label="Rent" className="!text-gray-300" ></Input>
+        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, insurance: parseFloat(e.target.value) })} value={newAssumptions.insurance} type="number" variant="outlined" label="Insurance" className="!text-gray-300" ></Input>
+        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, maintenance_fee: parseFloat(e.target.value) })} value={newAssumptions.maintenance_fee} type="number" variant="outlined" label="Maintenance Fee (Month)" className="!text-gray-300" ></Input>
+        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, utilities: parseFloat(e.target.value) })} value={newAssumptions.utilities} type="number" variant="outlined" label="Utilities" className="!text-gray-300" ></Input>
         <Tooltip content={"Comma Separated"} className="capitalize bg-gray-900 p-2">
           <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, other_expenses: e.target.value.split(",").map((item: string) => (item ? parseFloat(item) : 0)) })} value={otherExpensePlaceholder.join(",")} variant="outlined" label="Other Expenses (Month)" className="!text-gray-300"></Input>
         </Tooltip>
-        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, property_tax: e.target.value })} value={newAssumptions.property_tax} type="number" variant="outlined" label="Property Tax (Year)" className="!text-gray-300" ></Input>
-        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, repairs: e.target.value })} value={newAssumptions.repairs} type="number" variant="outlined" label="Repairs %" className="!text-gray-300" ></Input>
+        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, property_tax: parseFloat(e.target.value) })} value={newAssumptions.property_tax} type="number" variant="outlined" label="Property Tax (Year)" className="!text-gray-300" ></Input>
+        <Input onChange={(e) => setNewAssumptions({ ...newAssumptions, repairs: parseFloat(e.target.value) })} value={newAssumptions.repairs} type="number" variant="outlined" label="Repairs %" className="!text-gray-300" ></Input>
       </div >
     </div>
   )
