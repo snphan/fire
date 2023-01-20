1158cb40b9803fd2ff0143dbb2dd475e9ea3c52c
Steven Phan <snphan@ualberta.ca>
2023-01-19 22:24:01 -0700

    fix dev to work after making prod changes

--- upload/src/app.ts
+++ upload/src/app.ts
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 import cors from 'cors';
 import https from 'https';
 import fs from 'fs';
+import { NODE_ENV } from './config';
 
 const app = express();
 
@@ -30,13 +31,19 @@ app.post("/upload", upload.array('file'), (req: any, res) => {
   res.json({ status: "success", filenames: names });
 })
 
-https
+if (NODE_ENV === "production") {
+  https
     .createServer(
-        {
-            key: fs.readFileSync("/etc/ssl/live/firecash.app/privkey.pem"),
-            cert: fs.readFileSync("/etc/ssl/live/firecash.app/fullchain.pem"),
-        },
-        app
+      {
+        key: fs.readFileSync("/etc/ssl/live/firecash.app/privkey.pem"),
+        cert: fs.readFileSync("/etc/ssl/live/firecash.app/fullchain.pem"),
+      },
+      app
     ).listen(4000, () => {
-        console.log("Upload server is running at port 4000");
+      console.log("Upload server is running at port https://localhost:4000");
     });
+} else {
+  app.listen(4000, () => {
+    console.log("Upload server is running on port http://localhost:4000")
+  });
+}
