efdea3be50e87ad45e7ed22c280c18f77dceb9cf
Steven Phan <snphan@ualberta.ca>
2023-01-25 21:25:51 -0700

    move balance calculation logic to totalbalance component

--- client/src/components/Dashboard/TotalBalance.tsx
+++ client/src/components/Dashboard/TotalBalance.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import { CurrencyContext } from '@/Context';
+import React, { useContext, useEffect, useState } from 'react';
 import { Loading } from '../Loading';
 
 
-export function TotalBalance({ loading, totalBalance }: any) {
-  const currencyFormatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
+export function TotalBalance({ loading, balanceData }: any) {
+
+  const [totalBalance, setTotalBalance] = useState<number | undefined>(undefined);
+  const currencyFormatter = useContext(CurrencyContext);
+
+  useEffect(() => {
+    if (balanceData) {
+      setTotalBalance(balanceData.getBalance.balance.reduce(
+        (a: number, b: any) => {
+          if (b.balances.iso_currency_code === 'USD') {
+            return a + b.balances.current * 1.3;
+          } else {
+            return a + b.balances.current;
+          }
+        }, 0));
+    }
+  })
+
   return (
 
     <div className="flex flex-col bg-zinc-900 h-52 p-3 m-4 rounded-xl shadow-xl">
