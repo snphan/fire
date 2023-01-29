e95c33038230c7c3d9760a73b1522fef1920f138
Steven Phan <snphan@ualberta.ca>
2023-01-28 23:59:13 -0700

    make the sync method work with node-cron

--- server/src/repositories/plaid.repository.ts
+++ server/src/repositories/plaid.repository.ts
@@ -53,7 +53,8 @@ export default class PlaidRepository {
    */
   public async syncTransactions(user: User, plaidClient: PlaidApi) {
     const findPlaidInfo = await this.getPlaidInfoByUser(user);
-    if (!findPlaidInfo.length) throw new HttpException(409, "User has not connected to an Account through Plaid");
+    // if (!findPlaidInfo.length) throw new HttpException(409, "User has not connected to an Account through Plaid");
+    if (!findPlaidInfo.length) { console.log("No plaid connection"); return; }
 
     const syncedTransactions = {};  // Store updates in memory until written to DB. 
     const newCursors = {};          // Update the cursors after txns have been updated (if crash, we can redo the sync)
@@ -62,54 +63,59 @@ export default class PlaidRepository {
     // Fetch the new transactions.
     for (const plaidInfo of findPlaidInfo) {
       syncedTransactions[plaidInfo.institution_name] = {};
-      if (plaidInfo.products.includes(Products.Transactions)) {
-        let cursor = plaidInfo.txn_cursor;
-        let added = [];       // New added txns
-        let modified = [];    // Txns that were modified
-        let removed = [];     // Txns that were removed
-        let hasMore = true;
-        while (hasMore) {
-          const request = {
-            access_token: this.decryptAccessToken(plaidInfo.access_token),
-            cursor: cursor,
-            options: {
-              include_personal_finance_category: true
-            }
-          };
-          const response = await plaidClient.transactionsSync(request);
-          const data = response.data;
-          added = added.concat(data.added);
-          modified = modified.concat(data.modified);
-          removed = removed.concat(data.removed);
-          hasMore = data.has_more;
-          cursor = data.next_cursor;
+      try {
+        if (plaidInfo.products.includes(Products.Transactions)) {
+          let cursor = plaidInfo.txn_cursor;
+          let added = [];       // New added txns
+          let modified = [];    // Txns that were modified
+          let removed = [];     // Txns that were removed
+          let hasMore = true;
+          while (hasMore) {
+            const request = {
+              access_token: this.decryptAccessToken(plaidInfo.access_token),
+              cursor: cursor,
+              options: {
+                include_personal_finance_category: true
+              }
+            };
+            const response = await plaidClient.transactionsSync(request);
+            const data = response.data;
+            added = added.concat(data.added);
+            modified = modified.concat(data.modified);
+            removed = removed.concat(data.removed);
+            hasMore = data.has_more;
+            cursor = data.next_cursor;
+          }
+          newCursors[plaidInfo.institution_name] = cursor;
+          syncedTransactions[plaidInfo.institution_name]["added"] = added.map((txn) => this.plaidTransactionToEntity(plaidInfo, txn))
+          syncedTransactions[plaidInfo.institution_name]["modified"] = modified.map((txn) => this.plaidTransactionToEntity(plaidInfo, txn));
+          syncedTransactions[plaidInfo.institution_name]["removed"] = removed.map((txn) => this.plaidTransactionToEntity(plaidInfo, txn));
         }
-        newCursors[plaidInfo.institution_name] = cursor;
-        syncedTransactions[plaidInfo.institution_name]["added"] = added.map((txn) => this.plaidTransactionToEntity(plaidInfo, txn))
-        syncedTransactions[plaidInfo.institution_name]["modified"] = modified.map((txn) => this.plaidTransactionToEntity(plaidInfo, txn));
-        syncedTransactions[plaidInfo.institution_name]["removed"] = removed.map((txn) => this.plaidTransactionToEntity(plaidInfo, txn));
-      }
 
-      if (plaidInfo.products.includes(Products.Investments)) {
-        const startDate = plaidInfo.invest_txn_update_date;
-        const today = dayjs().format('YYYY-MM-DD');
-        const configs = {
-          access_token: this.decryptAccessToken(plaidInfo.access_token),
-          start_date: startDate,
-          end_date: today,
-        };
+        if (plaidInfo.products.includes(Products.Investments)) {
+          const startDate = plaidInfo.invest_txn_update_date;
+          const today = dayjs().format('YYYY-MM-DD');
+          const configs = {
+            access_token: this.decryptAccessToken(plaidInfo.access_token),
+            start_date: startDate,
+            end_date: today,
+          };
 
-        const response = await plaidClient.investmentsTransactionsGet(configs);
-        const data = response.data;
-        syncedTransactions[plaidInfo.institution_name]["investment_transactions"] = data.investment_transactions.map((txn) => this.plaidInvestTransactionToEntity(plaidInfo, txn));
-        newInvestUpdateDates[plaidInfo.institution_name] = today;
+          const response = await plaidClient.investmentsTransactionsGet(configs);
+          const data = response.data;
+          syncedTransactions[plaidInfo.institution_name]["investment_transactions"] = data.investment_transactions.map((txn) => this.plaidInvestTransactionToEntity(plaidInfo, txn));
+          newInvestUpdateDates[plaidInfo.institution_name] = today;
+        }
+      } catch (err) {
+        console.log(err.response.statusText);
+        return;
       }
     }
 
     // Write to DB
     console.log("Writing to the DB!");
-    console.log(syncedTransactions["TD Canada Trust"]["added"][0]);
-    console.log(syncedTransactions["TD Canada Trust - WebBroker"]["investment_transactions"][0]);
+    // console.log(syncedTransactions["TD Canada Trust"]["added"][0]);
+    // console.log(syncedTransactions["TD Canada Trust - WebBroker"]["investment_transactions"][0]);
     // console.log(newCursors);
     // console.log(newInvestUpdateDates);
 
