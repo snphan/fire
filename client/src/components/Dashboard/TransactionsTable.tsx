7f1842358d294f3d9773476746779cd141d3e028
Steven Phan <snphan@ualberta.ca>
2023-02-02 00:11:24 -0700

    format table to dollars

--- client/src/components/Dashboard/TransactionsTable.tsx
+++ client/src/components/Dashboard/TransactionsTable.tsx
@@ -1,6 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { CurrencyContext } from '@/Context';
 
 export function TransactionsTable({ className, allTransactions }: any) {
+
+  const currencyFormatter = useContext(CurrencyContext);
 
   return (
     <button className={className}>
@@ -11,18 +14,18 @@ export function TransactionsTable({ className, allTransactions }: any) {
             <table className="w-full h-full flex flex-col">
               <thead className="rounded-tl-xl rounded-tr-xl sticky top-0 bg-zinc-800 w-full">
                 <tr className="table text-zinc-500 text-left w-full">
-                  <th className="w-2/5 p-3 py-4">Name</th>
-                  <th className="w-2/5 p-3 py-4">Date</th>
-                  <th className="w-1/5 p-3 py-4">Amount</th>
+                  <th className="w-5/12 p-3 py-4">Name</th>
+                  <th className="w-3/12 p-3 py-4">Date</th>
+                  <th className="w-4/12 p-3 py-4">Amount</th>
                 </tr>
               </thead>
               <tbody className="block overflow-y-auto h-96 grow text-zinc-400 text-left scrollbar-thumb-zinc-700 scrollbar-thin scrollbar-track-transparent scrollbar-thumb-rounded-xl">
                 {allTransactions?.slice().sort((a: any, b: any) => (new Date(b.date)).getTime() - (new Date(a.date)).getTime()).map((txn: any) => {
                   return (
                     <tr key={txn.transaction_id} className="transition-all duration-300 hover:bg-sky-200 hover:bg-opacity-10">
-                      <td className="w-2/5 border-b border-zinc-700 p-3">{txn.name}</td>
-                      <td className="w-2/5 border-b border-zinc-700 p-3">{txn.date.split("T")[0]}</td>
-                      <td className="w-1/5 border-b border-zinc-700 p-3">{txn.amount}</td>
+                      <td className="w-5/12 border-b border-zinc-700 p-3">{txn.name}</td>
+                      <td className="w-3/12 border-b border-zinc-700 p-3">{txn.date.split("T")[0]}</td>
+                      <td className="w-4/12 border-b border-zinc-700 p-3">{currencyFormatter.format(Math.abs(txn.amount))}</td>
                     </tr>
                   )
                 })}
