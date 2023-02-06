7e3af5cc2f189549f60e007932c199b5f423d409
Steven Phan <snphan@ualberta.ca>
2023-02-05 22:42:54 -0700

    fix date ranges (b4 jan includes feb-01)

--- client/src/components/Dashboard/TransactionsTable.tsx
+++ client/src/components/Dashboard/TransactionsTable.tsx
@@ -1,11 +1,12 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { CurrencyContext, DashboardContext } from '@/Context';
 import { Tooltip } from '@material-tailwind/react';
 
 export function TransactionsTable({ className, allTransactions, allInvestTransactions, filters }: any) {
 
   const currencyFormatter = useContext(CurrencyContext);
-  const dashboardContext = useContext(DashboardContext);
+
+  const [filteredTxn, setFilteredTxn] = useState<any>(undefined);
 
   const getFilteredItems = () => {
     let allTxn = allTransactions.slice(); // Slice so that we can sort
@@ -16,8 +17,8 @@ export function TransactionsTable({ className, allTransactions, allInvestTransac
       allInvestTxn = allInvestTxn.filter((txn: any) => new Date(txn.date) >= new Date(startDate + "T00:00:00.000Z"));
     }
     if (endDate) {
-      allTxn = allTxn.filter((txn: any) => new Date(txn.date) <= new Date(endDate + "T00:00:00.000Z"));
-      allInvestTxn = allInvestTxn.filter((txn: any) => new Date(txn.date) <= new Date(endDate + "T00:00:00.000Z"));
+      allTxn = allTxn.filter((txn: any) => new Date(txn.date) < new Date(endDate + "T00:00:00.000Z"));
+      allInvestTxn = allInvestTxn.filter((txn: any) => new Date(txn.date) < new Date(endDate + "T00:00:00.000Z"));
     }
     if (categories) {
       allTxn = allTxn.filter((txn: any) => categories.includes(txn.category));
@@ -27,16 +28,29 @@ export function TransactionsTable({ className, allTransactions, allInvestTransac
     }
     if (categories?.includes('DIVIDENDS')) {
       allInvestTxn = allInvestTxn.filter((item: any) => item.type === "cash" && !item.name.match(/CONTRIBUTION/));
+      allTxn = allTxn.concat(allInvestTxn);
     }
 
     return allTxn.sort((a: any, b: any) => (new Date(b.date)).getTime() - (new Date(a.date)).getTime());
   }
 
+  useEffect(() => {
+    if (allTransactions && allInvestTransactions) setFilteredTxn(getFilteredItems());
+  }, [filters])
+
+  const getTotal = () => {
+    return filteredTxn?.reduce((a: number, b: any) => a + Math.abs(parseFloat(b.amount)), 0)
+  }
 
   return (
     <button className={className}>
-      <div className="flex flex-col h-full items-start">
-        <div className="mb-5 text-sm font-bold">Transactions Table</div>
+      <div className="flex flex-col h-full items-between">
+        <div className="flex justify-between">
+          <div className="mb-5 text-sm font-bold">Transactions Table</div>
+          {getTotal() &&
+            <div className="mb-5 text-md font-bold text-sky-500">{currencyFormatter.format(getTotal())}</div>
+          }
+        </div>
         <div className="p-4 flex flex-col grow h-10 w-full">
           <div className="grow w-full ">
             <table className="w-full h-full flex flex-col">
@@ -48,11 +62,16 @@ export function TransactionsTable({ className, allTransactions, allInvestTransac
                 </tr>
               </thead>
               <tbody className="block overflow-y-auto h-96 grow text-zinc-400 text-left scrollbar-thumb-zinc-700 scrollbar-thin scrollbar-track-transparent scrollbar-thumb-rounded-xl">
-                {(allTransactions && allInvestTransactions) && getFilteredItems().map((txn: any) => {
+                {filteredTxn?.map((txn: any) => {
                   return (
                     <tr key={txn.transaction_id || txn.investment_transaction_id} className="transition-all duration-300 hover:bg-sky-200 hover:bg-opacity-10">
                       <td className="w-5/12 border-b border-zinc-700 p-3">
-                        <Tooltip content={txn.category.split('_').map((substring: string) => substring[0].toUpperCase() + substring.slice(1).toLowerCase()).join(" ")} className="capitalize bg-gray-900 p-2">
+                        <Tooltip
+                          content={
+                            txn.category?.split('_').map((substring: string) => substring[0].toUpperCase() + substring.slice(1).toLowerCase()).join(" ")
+                            || txn.type?.split('_').map((substring: string) => substring[0].toUpperCase() + substring.slice(1).toLowerCase()).join(" ")
+                          }
+                          className="capitalize bg-gray-900 p-2">
                           {txn.name}
                         </Tooltip>
                       </td>
