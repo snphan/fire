ef888949a9bba8050995f529e11b2537f439dc66
Steven Phan <snphan@ualberta.ca>
2023-02-09 21:53:35 -0700

    truncate long adresses

--- client/src/components/REListItem.tsx
+++ client/src/components/REListItem.tsx
@@ -3,7 +3,7 @@ import { REACT_APP_MEDIA_HOST } from '@/config';
 
 export function REListItem({ REInfo, onClick, disabled, className }: any) {
   return (
-    <div className={(className ? className + " " : "") + (disabled ? "" : "hover:scale-105 cursor-pointer ") + "flex lg:p-5 rounded-xl m-2 lg:m-5 min-h-36 bg-zinc-900 shadow-2xl"}
+    <div className={(className ? className + " " : "") + (disabled ? "" : "hover:scale-105 cursor-pointer ") + "flex lg:p-5 rounded-xl m-2 lg:m-5 h-28 max-w-screen lg:h-auto lg:min-h-36 bg-zinc-900 shadow-2xl"}
       onClick={onClick}
     >
       <img className="lg:rounded-xl rounded-l-xl w-28 h-full lg:w-28 lg:h-28" src={`${REACT_APP_MEDIA_HOST}/media/${REInfo.picture_links.length ? REInfo.picture_links[0] : "a04e34f6-85fa-447a-b316-89363851e9c6-fire logo.png"}`} alt="" />
@@ -15,7 +15,9 @@ export function REListItem({ REInfo, onClick, disabled, className }: any) {
           <span className="text-sm lg:text-xl material-icons">bathtub</span>
           <div className="text-sm lg:text-xl">{REInfo.bathrooms}</div>
         </div>
-        <h4>{REInfo.address}, {REInfo.city}, {REInfo.province}</h4>
+        <h4 className={`truncate lg:w-auto`}
+          style={{ width: `calc(${window.screen.width * 0.9}px - 7rem)` }}
+        >{REInfo.address}, {REInfo.city}, {REInfo.province}</h4>
       </div>
     </div>
   )
