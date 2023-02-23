c3cb163531fcad1a474c283bb4a7034c2b5f99a8
Steven Phan <snphan@ualberta.ca>
2023-02-22 23:51:29 -0700

    add a prompt if no bank account is linked

--- client/src/pages/Goals.tsx
+++ client/src/pages/Goals.tsx
@@ -171,88 +171,95 @@ export function Goals({ }: any) {
               style={{ backgroundImage: `url('${createGoalData.name.toLowerCase()}.png')` }}
             >{createGoalData.name ? createGoalData.name : "Add a goal!"}</DialogHeader>
             <DialogBody className="text-zinc-400 font-ubuntu grow">
-              <Carousel className="w-full" noLoop arrowPos="bottom">
-                <Carousel.Item className="!justify-start flex-col">
-                  <h4 className="text-lg text-center font-thin leading-loose mt-5 mb-14">Please name your goal:</h4>
-                  <Input
-                    value={createGoalData.name}
-                    onChange={(e: any) => setCreateGoalData({ ...createGoalData, name: e.target.value })}
-                    size="lg" variant="standard" labelProps={{ className: "!text-zinc-500" }} label="" className="!text-zinc-300 text-center font-thin font-[Ubuntu] text-2xl" />
-                </Carousel.Item>
-
-                <Carousel.Item className="!justify-start flex-col">
-                  <div className="flex flex-col m-4">
-                    <h4 className="text-lg text-center font-thin leading-loose mt-4 mb-10">How much would you like to save?</h4>
+              {isBankLinked ?
+                <Carousel className="w-full" noLoop arrowPos="bottom">
+                  <Carousel.Item className="!justify-start flex-col">
+                    <h4 className="text-lg text-center font-thin leading-loose mt-5 mb-14">Please name your goal:</h4>
                     <Input
-                      value={createGoalData.goal_amount}
-                      onChange={(e: any) => setCreateGoalData({ ...createGoalData, goal_amount: e.target.value })}
-                      size="lg" variant="standard" labelProps={{ className: "!text-zinc-500" }} icon={<span className="material-icons text-zinc-300">attach_money</span>} className="!text-zinc-300 font-thin font-[Ubuntu] text-2xl" />
-                  </div>
-                </Carousel.Item>
+                      value={createGoalData.name}
+                      onChange={(e: any) => setCreateGoalData({ ...createGoalData, name: e.target.value })}
+                      size="lg" variant="standard" labelProps={{ className: "!text-zinc-500" }} label="" className="!text-zinc-300 text-center font-thin font-[Ubuntu] text-2xl" />
+                  </Carousel.Item>
 
-                <Carousel.Item className="!justify-start flex-col">
-                  <div className="flex flex-col m-4">
-                    <h4 className="text-lg text-center font-thin leading-loose mt-4 mb-10">When do you need to complete this goal?</h4>
-                    <Input
-                      type="date"
-                      value={createGoalData.due_date}
-                      onChange={(e: any) => setCreateGoalData({ ...createGoalData, due_date: e.target.value })}
-                      size="lg" variant="standard" labelProps={{ className: "!text-zinc-500" }} className="!text-zinc-300 font-[Ubuntu] font-thin text-2xl" />
-                  </div>
-                </Carousel.Item>
-
-                <Carousel.Item className="!justify-start flex-col">
-                  <div className="flex flex-col m-4">
-                    <h4 className="text-lg text-center font-thin leading-loose mt-4 mb-6">Select the account(s) to track:</h4>
-                    <div className="h-3/4 p-2 overflow-auto">
-                      {balanceData?.getAccounts.accounts.filter((account: any) => account.name.toLowerCase().match(/(chequing)|(saving)/)).map((account: any) => {
-                        return (<Checkbox
-                          onChange={(e) => {
-                            const accountName = e.target.id;
-                            const { track_accounts } = createGoalData;
-                            if (createGoalData.track_accounts.includes(accountName)) {
-                              setCreateGoalData({ ...createGoalData, track_accounts: track_accounts.filter((item: string) => item !== accountName) });
-                            } else {
-                              setCreateGoalData({ ...createGoalData, track_accounts: track_accounts.concat([accountName]) });
-                            }
-                          }}
-                          checked={createGoalData.track_accounts.includes(account.name)}
-                          key={account.account_id} id={account.name} label={account.name} labelProps={{ className: "text-sm text-zinc-300" }} />)
-                      }
-                      )}
+                  <Carousel.Item className="!justify-start flex-col">
+                    <div className="flex flex-col m-4">
+                      <h4 className="text-lg text-center font-thin leading-loose mt-4 mb-10">How much would you like to save?</h4>
+                      <Input
+                        value={createGoalData.goal_amount}
+                        onChange={(e: any) => setCreateGoalData({ ...createGoalData, goal_amount: e.target.value })}
+                        size="lg" variant="standard" labelProps={{ className: "!text-zinc-500" }} icon={<span className="material-icons text-zinc-300">attach_money</span>} className="!text-zinc-300 font-thin font-[Ubuntu] text-2xl" />
                     </div>
-                  </div>
-                </Carousel.Item>
+                  </Carousel.Item>
 
-
-                <Carousel.Item className="!justify-start flex-col">
-                  <div className="flex flex-col m-4">
-                    <h4 className="text-lg text-center font-thin leading-loose mt-4 mb-2">How much do you have saved for this goal already?</h4>
-                    <div className="text-xs text-center font-thin leading-loose mb-8">
-                      (Max Amount {
-                        currencyFormatter.format(
-                          balanceData?.getAccounts.accounts.filter((account: any) => createGoalData.track_accounts.includes(account.name)).reduce((a: number, b: any) => a + b.balances.current, 0)
-                        )
-                      } from all <span className="material-icons text-sm">check_box</span> accounts)
+                  <Carousel.Item className="!justify-start flex-col">
+                    <div className="flex flex-col m-4">
+                      <h4 className="text-lg text-center font-thin leading-loose mt-4 mb-10">When do you need to complete this goal?</h4>
+                      <Input
+                        type="date"
+                        value={createGoalData.due_date}
+                        onChange={(e: any) => setCreateGoalData({ ...createGoalData, due_date: e.target.value })}
+                        size="lg" variant="standard" labelProps={{ className: "!text-zinc-500" }} className="!text-zinc-300 font-[Ubuntu] font-thin text-2xl" />
                     </div>
-                    <Input
-                      value={createGoalData.amount_saved}
-                      onChange={(e: any) => setCreateGoalData({ ...createGoalData, amount_saved: e.target.value })}
-                      size="lg" variant="standard" labelProps={{ className: "!text-zinc-500" }} icon={<span className="material-icons text-zinc-300">attach_money</span>} className="!text-zinc-300 font-[Ubuntu] font-thin text-2xl" />
-                  </div>
-                </Carousel.Item>
+                  </Carousel.Item>
 
-                <Carousel.Item className="flex-col">
-                  <div className="-mt-6">
-                    <h4 className="text-lg text-center font-thin leading-loose mb-2">✨ Your Savings Target ✨</h4>
-                    <div className="text-3xl text-center lg:text-4xl font-bold text-sky-500">{calculateSavingsRate()}</div>
-                  </div>
-                </Carousel.Item>
-                <Carousel.Item className="flex-col">
-                  <h4 className="text-lg text-center font-thin leading-loose mb-10">Tap the check to start saving!</h4>
-                  <button onClick={handleClickCreateGoal} className="animate-bounce font-bold w-14 h-14 rounded-full drop-shadow-strong bg-gradient-to-tr from-sky-400 to-sky-700 text-zinc-200 flex justify-center items-center"><span className="material-icons">check</span></button>
-                </Carousel.Item>
-              </Carousel>
+                  <Carousel.Item className="!justify-start flex-col">
+                    <div className="flex flex-col m-4">
+                      <h4 className="text-lg text-center font-thin leading-loose mt-4 mb-6">Select the account(s) to track:</h4>
+                      <div className="h-3/4 p-2 overflow-auto">
+                        {balanceData?.getAccounts.accounts.filter((account: any) => account.name.toLowerCase().match(/(chequing)|(saving)/)).map((account: any) => {
+                          return (<Checkbox
+                            onChange={(e) => {
+                              const accountName = e.target.id;
+                              const { track_accounts } = createGoalData;
+                              if (createGoalData.track_accounts.includes(accountName)) {
+                                setCreateGoalData({ ...createGoalData, track_accounts: track_accounts.filter((item: string) => item !== accountName) });
+                              } else {
+                                setCreateGoalData({ ...createGoalData, track_accounts: track_accounts.concat([accountName]) });
+                              }
+                            }}
+                            checked={createGoalData.track_accounts.includes(account.name)}
+                            key={account.account_id} id={account.name} label={account.name} labelProps={{ className: "text-sm text-zinc-300" }} />)
+                        }
+                        )}
+                      </div>
+                    </div>
+                  </Carousel.Item>
+
+
+                  <Carousel.Item className="!justify-start flex-col">
+                    <div className="flex flex-col m-4">
+                      <h4 className="text-lg text-center font-thin leading-loose mt-4 mb-2">How much do you have saved for this goal already?</h4>
+                      <div className="text-xs text-center font-thin leading-loose mb-8">
+                        (Max Amount {
+                          currencyFormatter.format(
+                            balanceData?.getAccounts.accounts.filter((account: any) => createGoalData.track_accounts.includes(account.name)).reduce((a: number, b: any) => a + b.balances.current, 0)
+                          )
+                        } from all <span className="material-icons text-sm">check_box</span> accounts)
+                      </div>
+                      <Input
+                        value={createGoalData.amount_saved}
+                        onChange={(e: any) => setCreateGoalData({ ...createGoalData, amount_saved: e.target.value })}
+                        size="lg" variant="standard" labelProps={{ className: "!text-zinc-500" }} icon={<span className="material-icons text-zinc-300">attach_money</span>} className="!text-zinc-300 font-[Ubuntu] font-thin text-2xl" />
+                    </div>
+                  </Carousel.Item>
+
+                  <Carousel.Item className="flex-col">
+                    <div className="-mt-6">
+                      <h4 className="text-lg text-center font-thin leading-loose mb-2">✨ Your Savings Target ✨</h4>
+                      <div className="text-3xl text-center lg:text-4xl font-bold text-sky-500">{calculateSavingsRate()}</div>
+                    </div>
+                  </Carousel.Item>
+                  <Carousel.Item className="flex-col">
+                    <h4 className="text-lg text-center font-thin leading-loose mb-10">Tap the check to start saving!</h4>
+                    <button onClick={handleClickCreateGoal} className="animate-bounce font-bold w-14 h-14 rounded-full drop-shadow-strong bg-gradient-to-tr from-sky-400 to-sky-700 text-zinc-200 flex justify-center items-center"><span className="material-icons">check</span></button>
+                  </Carousel.Item>
+                </Carousel>
+                :
+                <div className="flex flex-col justify-start items-center">
+                  <div className="font-thin text-center mb-10">Please link your bank account in Dashboard</div>
+                  <Button onClick={handleOpenAddGoal} className="font-[Ubuntu]">Back</Button>
+                </div>
+              }
             </DialogBody>
           </>
         }
